b0bc4bf7606d18b04bbd78ab1f9f3c1193ec0893
rexj8 <rachit.gla_cs18@gla.ac.in>
2020-07-08 12:25:21 +0530

    getting data from http://glauniversity.in:8085/

---
+++ get_from_web.py
@@ -0,0 +1,8 @@
+import requests
+from bs4 import BeautifulSoup
+
+r = requests.get("http://glauniversity.in:8085/")
+htmlContent = r.content
+# print(htmlContent)
+
+# soup = BeautifulSoup(htmlContent,'html.parser')
