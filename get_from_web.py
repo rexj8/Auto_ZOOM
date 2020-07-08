395c9ae29c37ad64b6248b75f34ecbca24fc3e99
rexj8 <rachit.gla_cs18@gla.ac.in>
2020-07-08 12:41:18 +0530

    accessing text from http://glauniversity.in:8085/

--- get_from_web.py
+++ get_from_web.py
@@ -9,4 +9,10 @@ soup = BeautifulSoup(htmlContent,'html.parser')
 # print(soup.prettify)
 
 title = soup.title
-print(title.string)
+# print(title.string)
+
+anch = soup.find_all('a')
+# print(anch)
+
+print(soup.find('p').get_text())
+print(soup.get_text())
